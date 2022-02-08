56081a4c371e7ea02473d6e1b082b73a6eb26c84
unknown <finnneron1@gmail.com>
2022-02-08 13:17:17 +0300

    kotlin classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,7 @@
 fun main(args: Array<String>) {
     println("Hello World!")
     println("Program arguments: ${args.joinToString()}")
-    deno()
 }
-fun deno(){
-    for(i in 0..10){
-        print(i)
-    }
+class Rectangle(var height: Double, var length: Double) {
+    var perimeter = (height + length) * 2
 }
