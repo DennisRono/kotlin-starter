a4425fe8779ba6a57bb258a9364d9f48bb8d208c
unknown <finnneron1@gmail.com>
2022-02-10 20:23:12 +0300

    kotlin atm machine

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,35 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    //enquire user to login or to create an account
+    starting()
+    val (a, b) = readLine()!!.split(' ')
+    println(a.toInt() + b.toInt())
 }
-class Rectangle(var height: Double, var length: Double) {
-    var perimeter = (height + length) * 2
+fun starting() {
+    println("WELCOME TO OUR ATM")
+    println("1. Login to your account")
+    login()
+    println("2. Register for a new account")
+    register()
+}
+fun login() {
+    println("LOGIN")
+    print("Enter your Username or Email")
+    print("Enter your Password")
+    dashboard()
+}
+fun register(){
+    println("REGISTER")
+    print("Enter your username")
+    print("Enter your email address")
+    print("Enter your password")
+    print("Confirm your Password")
+    login()
+}
+fun dashboard() {
+    println("WELCOME TO OUR ATM")
+    println("what services do you wish to use")
+    println("1. deposit your cash")
+    println("2. check your account balance")
+    println("3. withdraw from your account")
+    println("4. change your account balance")
 }
